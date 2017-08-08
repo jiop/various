b7d3589c9ebbbac94e54109ea3522d9c2b3c80b1
Jeremie IOP <jeremie.iop@displaysweet.com>
2017-08-08 15:44:50 +1000

    more gameoflife

--- gameoflife/main.go
+++ gameoflife/main.go
@@ -2,56 +2,102 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
 	"math/rand"
 	"strings"
 	"time"
 )
 
-const MAX_Y = 15
-const MAX_X = 100
-const NB_CELLS = 50
+const maxY = 15
+const maxX = 50
+const nbCells = 10
 
-type Board struct {
-	grid    [2][MAX_X][MAX_Y]bool
-	current int
+type board struct {
+	grid        [2][maxX][maxY]bool
+	currentGrid int
+	printer     printer
 }
 
-type Cell struct {
-	x int
-	y int
+type printer interface {
+	print(*board)
 }
 
-func newBoard() *Board {
-	return &Board{}
-}
+type termPrinter struct{}
 
-func (b *Board) seed() {
-	for i := 0; i < NB_CELLS; i++ {
-		b.grid[b.current][rand.Intn(MAX_X)][rand.Intn(MAX_Y)] = true
-	}
-}
-
-func (b *Board) print() {
-	for j := 0; j < MAX_Y; j++ {
-		for i := 0; i < MAX_X; i++ {
-			if b.grid[b.current][i][j] {
-				fmt.Print("x")
+func (termPrinter) print(b *board) {
+	const clrR = "\x1b[31;1m"
+	const clrG = "\x1b[32;1m"
+	const clrN = "\x1b[0m"
+	for j := 0; j < maxY; j++ {
+		for i := 0; i < maxX; i++ {
+			if b.current()[i][j] {
+				fmt.Printf("%sx%s", clrR, clrN)
 			} else {
-				fmt.Print(" ")
+				fmt.Printf("%so%s", clrG, clrN)
 			}
 		}
 		fmt.Println()
 	}
 }
 
-func (b *Board) getAliveNeighboursCount(c Cell) int {
+type floydSteinbergPrinter struct{}
+
+func (floydSteinbergPrinter) print(b *board) {
+	im := image.NewGray(image.Rectangle{Max: image.Point{X: maxX, Y: maxY}})
+	for i := 0; i < maxX; i++ {
+		for j := 0; j < maxY; j++ {
+			if b.getCellState(i, j) {
+				im.SetGray(i, j, color.Gray{Y: 0})
+			} else {
+				im.SetGray(i, j, color.Gray{Y: 35})
+			}
+		}
+	}
+	pi := image.NewPaletted(im.Bounds(), []color.Color{
+		color.Gray{Y: 255},
+		color.Gray{Y: 160},
+		color.Gray{Y: 70},
+		color.Gray{Y: 35},
+		color.Gray{Y: 0},
+	})
+	draw.FloydSteinberg.Draw(pi, im.Bounds(), im, image.ZP)
+	shade := []string{" ", "░", "▒", "▓", "█"}
+	for i, p := range pi.Pix {
+		fmt.Print(shade[p])
+		if (i+1)%maxX == 0 {
+			fmt.Print("\n")
+		}
+	}
+}
+
+func newBoard(p printer) *board {
+	return &board{printer: p}
+}
+
+func (b *board) current() [maxX][maxY]bool {
+	return b.grid[b.currentGrid]
+}
+
+func (b *board) seed() {
+	for i := 0; i < nbCells; i++ {
+		b.grid[b.currentGrid][rand.Intn(maxX)][rand.Intn(maxY)] = true
+	}
+}
+
+func (b *board) print() {
+	b.printer.print(b)
+}
+
+func (b *board) getAliveNeighboursCount(x int, y int) int {
 	alives := 0
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
 			if i == 0 && j == 0 {
 				continue
 			}
-			if b.getCellState(Cell{x: c.x + i, y: c.y + j}) {
+			if b.getCellState(x+i, y+j) {
 				alives++
 			}
 		}
@@ -59,46 +105,45 @@ func (b *Board) getAliveNeighboursCount(c Cell) int {
 	return alives
 }
 
-func (b *Board) getCellState(c Cell) bool {
-	if c.x < 0 || c.y < 0 || c.x >= MAX_X || c.y >= MAX_Y {
+func (b *board) getCellState(x int, y int) bool {
+	if x < 0 || y < 0 || x >= maxX || y >= maxY {
 		return false
 	}
-	return b.grid[b.current][c.x][c.y]
+	return b.current()[x][y]
 }
 
-func (b *Board) nextState(c Cell) bool {
-	alives := b.getAliveNeighboursCount(c)
-	if alives < 2 {
-		return false // underpopulation
+func (b *board) nextState(x int, y int) bool {
+	alives := b.getAliveNeighboursCount(x, y)
+	if alives < 2 || alives > 3 {
+		return false // underpopulation or overpopulation
 	}
-	if alives > 3 {
-		return false // overpopulation
-	}
-	if alives == 3 && !b.getCellState(c) {
+	if alives == 3 && !b.getCellState(x, y) {
 		return true // reproduction
 	}
 	return true // survives
 }
 
-func (b *Board) step() {
-	for j := 0; j < MAX_Y; j++ {
-		for i := 0; i < MAX_X; i++ {
-			b.grid[(b.current+1)%2][i][j] = b.nextState(Cell{x: i, y: j})
+func (b *board) tick() {
+	next := (b.currentGrid + 1) % 2
+	for j := 0; j < maxY; j++ {
+		for i := 0; i < maxX; i++ {
+			b.grid[next][i][j] = b.nextState(i, j)
 		}
 	}
-	b.current = (b.current + 1) % 2
+	b.currentGrid = next
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	b := newBoard()
+	// b := newBoard(&termPrinter{})
+	b := newBoard(&floydSteinbergPrinter{})
 	b.seed()
 
 	for {
 		select {
 		case <-time.After(200 * time.Millisecond):
-			b.step()
-			fmt.Println(strings.Repeat("#", MAX_Y))
+			b.tick()
+			fmt.Println(strings.Repeat("#", maxX))
 			b.print()
 		}
 	}
